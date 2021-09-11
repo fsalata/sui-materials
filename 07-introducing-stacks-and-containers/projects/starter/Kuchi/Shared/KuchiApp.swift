d3b122148d1bb5270482dc8772c17017518534b5
Fábio Salata <s2it_fsalata@uolinc.com>
2021-09-11 09:28:44 -0300

    chapters

--- 07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/KuchiApp.swift
+++ 07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/KuchiApp.swift
@@ -34,22 +34,22 @@ import SwiftUI
 
 @main
 struct KuchiApp: App {
-  let userManager = UserManager()
-  
-  init() {
-    userManager.load()
-  }
-
-  var body: some Scene {
-    WindowGroup {
-      RegisterView(keyboardHandler: KeyboardFollower())
-        .environmentObject(userManager)
+    let userManager = UserManager()
+    
+    init() {
+        userManager.load()
     }
-  }
+    
+    var body: some Scene {
+        WindowGroup {
+            StarterView()
+                .environmentObject(userManager)
+        }
+    }
 }
 
 struct KuchiApp_Previews: PreviewProvider {
-  static var previews: some View {
-    RegisterView(keyboardHandler: KeyboardFollower())
-  }
+    static var previews: some View {
+        RegisterView(keyboardHandler: KeyboardFollower())
+    }
 }
