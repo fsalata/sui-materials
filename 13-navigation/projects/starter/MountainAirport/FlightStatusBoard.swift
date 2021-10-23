9cc746459c302061959bcae8dc9a3c187914370f
Fábio Salata <s2it_fsalata@uolinc.com>
2021-10-23 10:44:28 -0300

    chapter 13

--- 13-navigation/projects/starter/MountainAirport/FlightStatusBoard.swift
+++ 13-navigation/projects/starter/MountainAirport/FlightStatusBoard.swift
@@ -32,14 +32,65 @@
 
 import SwiftUI
 
+struct FlightList: View {
+    var flights: [FlightInformation]
+
+    var body: some View {
+        List(flights, id: \.id) { flight in
+            NavigationLink(flight.statusBoardName, destination: FlightDetails(flight: flight))
+        }
+        .listStyle(PlainListStyle())
+    }
+}
+
 struct FlightStatusBoard: View {
-  var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-  }
+    var flights: [FlightInformation]
+
+    @State private var hidePast = false
+
+    @AppStorage("FlightStatusCurrentTab") var selectedTab = 1
+
+    var showFlights: [FlightInformation] {
+        hidePast ? flights.filter { $0.localTime >= Date() } : flights
+    }
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            FlightList(flights: showFlights.filter { $0.direction == .arrival })
+                .tabItem {
+                    Image("descending-airplane")
+                        .resizable()
+                    Text("Arrivals")
+                }
+                .tag(0)
+
+            FlightList(flights: showFlights)
+                .tabItem {
+                    Image(systemName: "airplane")
+                        .resizable()
+                    Text("All")
+                }
+                .tag(1)
+
+            FlightList(flights: showFlights.filter { $0.direction == .departure })
+                .tabItem {
+                    Image("ascending-airplane")
+                        .resizable()
+                    Text("Departures")
+                }
+                .tag(2)
+        }
+        .navigationTitle("Flight Status")
+        .toolbar {
+            Toggle("Hide Past", isOn: $hidePast)
+        }
+    }
 }
 
 struct FlightStatusBoard_Previews: PreviewProvider {
-  static var previews: some View {
-    FlightStatusBoard()
-  }
+    static var previews: some View {
+        NavigationView {
+            FlightStatusBoard(flights: FlightData.generateTestFlights(date: Date()))
+        }
+    }
 }
