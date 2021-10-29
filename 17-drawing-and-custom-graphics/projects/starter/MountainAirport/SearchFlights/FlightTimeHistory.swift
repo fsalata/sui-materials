2bde439a02236f533a6a6ac6be5bb5b095e2136b
Fábio Salata <s2it_fsalata@uolinc.com>
2021-10-29 14:10:14 -0300

    chapter 17

--- 17-drawing-and-custom-graphics/projects/starter/MountainAirport/SearchFlights/FlightTimeHistory.swift
+++ 17-drawing-and-custom-graphics/projects/starter/MountainAirport/SearchFlights/FlightTimeHistory.swift
@@ -33,38 +33,101 @@
 import SwiftUI
 
 struct FlightTimeHistory: View {
-  var flight: FlightInformation
+    var flight: FlightInformation
 
-  var body: some View {
-    ZStack {
-      Image("background-view")
-        .resizable()
-        .aspectRatio(contentMode: .fill)
-      VStack {
-        Text("On Time History for \(flight.statusBoardName)")
-          .font(.title2)
-          .padding(.top, 30)
-        ScrollView {
-          ForEach(flight.history, id: \.day) { history in
-            HStack {
-              Text("\(history.day) day(s) ago - \(history.flightDelayDescription)")
-                .padding()
-              Spacer()
-            }
-            .background(
-              Color.white.opacity(0.2)
-            )
-          }
+    let minuteRange = CGFloat(75)
+
+    func minuteLength(_ minutes: Int, proxy: GeometryProxy) -> CGFloat {
+        let pointsPerMinute = proxy.size.width / minuteRange
+        return CGFloat(abs(minutes)) * pointsPerMinute
+    }
+
+    func minuteOffset(_ minutes: Int, proxy: GeometryProxy) -> CGFloat {
+        let pointsPerMinute = proxy.size.width / minuteRange
+        let offset = minutes < 0 ? 15 + minutes : 15
+        return CGFloat(offset) * pointsPerMinute
+    }
+
+    func chartGradient(_ history: FlightHistory) -> Gradient {
+        if history.status == .canceled {
+            return Gradient(colors: [
+                Color.green,
+                Color.yellow,
+                Color.red,
+                Color(red: 0.5, green: 0, blue: 0)
+            ])
         }
-      }
-    }.foregroundColor(.white)
-  }
+
+        if history.timeDifference <= 0 {
+            return Gradient(colors: [Color.green])
+        }
+
+        if history.timeDifference <= 15 {
+            return Gradient(colors: [Color.green, Color.yellow])
+        }
+
+        return Gradient(colors: [Color.green, Color.yellow, Color.red])
+    }
+
+    func minuteLocation(_ minutes: Int, proxy: GeometryProxy) -> CGFloat {
+        let minMinutes = -15
+        let pointsPerMinute = proxy.size.width / minuteRange
+        let offset = CGFloat(minutes - minMinutes) * pointsPerMinute
+        return offset
+    }
+
+    var body: some View {
+        ZStack {
+            Image("background-view")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+            VStack {
+                Text("On Time History for \(flight.statusBoardName)")
+                    .font(.title2)
+                    .padding(.top, 30)
+                ScrollView {
+                    ForEach(flight.history, id: \.day) { history in
+                        HStack {
+                            Text("\(history.day) days ago")
+                                .frame(width: 110, alignment: .trailing)
+
+                            GeometryReader { proxy in
+                                Rectangle()
+                                    .fill(
+                                        LinearGradient(gradient: chartGradient(history),
+                                                       startPoint: .leading,
+                                                       endPoint: .trailing)
+                                    )
+                                    .frame(width: minuteLength(history.timeDifference, proxy: proxy))
+                                    .offset(x: minuteOffset(history.timeDifference, proxy: proxy))
+
+                                ForEach(-1..<6) { val in
+                                    Rectangle()
+                                        .stroke(val == 0 ? Color.white : Color.gray, lineWidth: 1.0)
+                                        .frame(width: 1)
+                                        .offset(x: minuteLocation(val * 10, proxy: proxy))
+                                }
+                            }
+                        }
+                        .padding()
+                        .background(
+                            Color.white.opacity(0.2)
+                        )
+                    }
+                }
+                HistoryPieChart(flightHistory: flight.history)
+                    .font(.footnote)
+                    .frame(width: 250, height: 250)
+                    .padding(5)
+            }
+        }.foregroundColor(.white)
+    }
 }
 
 struct FlightTimeHistory_Previews: PreviewProvider {
-  static var previews: some View {
-    FlightTimeHistory(
-      flight: FlightData.generateTestFlight(date: Date())
-    )
-  }
+    static var previews: some View {
+        FlightTimeHistory(
+            flight: FlightData.generateTestFlight(date: Date())
+        )
+    }
 }
